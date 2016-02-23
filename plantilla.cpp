5bad1dbc25670b0d4e19ff2d3218c68935417ff3
Pelllicer <Pelllicer@gmail.com>
2016-02-23 13:05:40 +0100

    Plantilla con cajetines y ejemplos de formato y definiciones

---
+++ plantilla.cpp
@@ -0,0 +1,61 @@
+/*****************************************************************
+FICHERO: matriz.c
+AUTOR: Sebastián Lopez
+FECHA: 17.II.2004
+DESCRIPCION: Implementación de las funciones de la librería de matrices.
+El fichero de enlace es matriz.h.
+*****************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.1415
+#define N_E 2.178281
+
+#define CUADRADO(X) ((X)*(X))
+
+int esDiagonal(matriz *a);
+int esSingular(matriz *b);
+
+/*************************************************************
+FUNCION: int invertirMatriz(Matriz in, Matriz out)
+ARGUMENTOS:
+Matriz in: Matriz que se quiere invertir.
+Matriz out:Matriz donde se sobrescribe la inversa si existe.
+RETORNO: Retornará un 1 en caso de que exista la matriz inversa,
+y un cero si la matriz es singular.
+DESCRIPCION: Calcula la inversa de la matriz in y la guarda en out si
+tiene éxito. Informa de la existencia de la inversa.
+************************************************************/
+void multiplicaMatriz(matriz mA,matriz mB, matriz mRes )
+{
+int i, j k;
+
+if(valor==5)
+  {
+  printf(“Correcto”);
+  valor=valor*10;
+  }
+}
+
+/*************************************************************
+Cajetín de la siguiente función
+**************************************************************/
+int EsDiagonal(matriz *mA)
+{
+switch(caracter)
+  {
+  case '0':
+    printf(“Cero”);
+  break;
+  case ’1’:
+  case ’2’:
+  case ’3’:
+    printf(“Mayor que cero”);
+    printf(“Menor que cuatro”);
+  break;
+  default:
+    printf(“Fuera de rango”);
+  };
+}
+/*** y así con el resto de funciones***/
